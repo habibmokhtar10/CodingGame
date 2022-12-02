41a8ec266a91b798546b3b65fa915ccc23fafc3d
habibmokhtar10 <habibmokthar@gmail.com>
2022-12-02 09:38:08 +0100

    Update cypress.config.js

--- cypress.config.js
+++ cypress.config.js
@@ -5,12 +5,21 @@ module.exports = defineConfig({
   viewportWidth: 1200,
   viewportHeight: 760,
   chromeWebSecurity: false,
+
+  reporter: "cypress-mochawesome-reporter",
+  reporterOptions: {
+    charts: true,
+    reportPageTitle: "custom-title",
+    embeddedScreenshots: true,
+    inlineAssets: true,
+    saveAllAttempts: false,
+  },
+
   e2e: {
     // We've imported your old cypress plugins here.
     // You may want to clean this up later by importing these.
     setupNodeEvents(on, config) {
-      return require("./cypress/plugins/index.js")(on, config);
+      require("cypress-mochawesome-reporter/plugin")(on);
     },
-    specPattern: "cypress/e2e/**/*.{js,jsx,ts,tsx}",
   },
 });
