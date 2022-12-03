aee2fe12e1cd413db977e9d7931fcfe49903bee4
habibmokhtar10 <habibmokthar@gmail.com>
2022-12-03 20:48:21 +0100

    2nd commit

--- cypress.config.js
+++ cypress.config.js
@@ -18,8 +18,6 @@ module.exports = defineConfig({
   e2e: {
     // We've imported your old cypress plugins here.
     // You may want to clean this up later by importing these.
-    setupNodeEvents(on, config) {
-      require("cypress-mochawesome-reporter/plugin")(on);
-    },
+    setupNodeEvents(on, config) {},
   },
 });
