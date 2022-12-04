7a0ce5d1046034a987b55078ef59d7c669abd352
habibmokhtar10 <habibmokthar@gmail.com>
2022-12-04 23:37:34 +0100

    4th commit

--- cypress.config.js
+++ cypress.config.js
@@ -1,4 +1,9 @@
 const { defineConfig } = require("cypress");
+const {
+  beforeRunHook,
+  afterRunHook,
+} = require("cypress-mochawesome-reporter/lib");
+
 const preprocessor = require("@badeball/cypress-cucumber-preprocessor");
 const browserify = require("@badeball/cypress-cucumber-preprocessor/browserify");
 
@@ -12,13 +17,37 @@ async function setupNodeEvents(on, config) {
 }
 
 module.exports = defineConfig({
+  reporter: "cypress-mochawesome-reporter",
+  reporter: "mochawesome",
+
+  reporterOptions: {
+    charts: true,
+    reportPageTitle: "custom-title",
+    embeddedScreenshots: true,
+    inlineAssets: true,
+    saveAllAttempts: false,
+    html: true,
+    json: true,
+  },
+  screenshotsFolder: "images",
   chromeWebSecurity: false,
   pageLoadTimeout: 200000,
   e2e: {
+    setupNodeEvents(on, config) {
+      on("before:run", async (details) => {
+        console.log("override before:run");
+        await beforeRunHook(details);
+      });
+
+      on("after:run", async () => {
+        console.log("override after:run");
+        await afterRunHook();
+      });
+      // require("cypress-mochawesome-reporter/plugin")(on);
+    },
+
     specPattern: "**/*.feature",
     supportFile: false,
     setupNodeEvents,
   },
 });
-
-
